63f41346e160b6f87e857126b60c1cea9cb3a721
Ir13570 <rikuisikawa11924@gmail.com>
2017-07-21 02:35:50 +0900

    first

---
+++ VB2017/Assets/Scripts/Ball_sc.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_sc : MonoBehaviour {
+
+	private GameObject ball, light, maincamera;
+	private Vector3 ball_v, ball_p;
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.FindGameObjectWithTag ("Ball");
+		light = GameObject.Find ("/Ball_light") as GameObject;
+		maincamera = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ball_v = ball.gameObject.GetComponent<Rigidbody> ().velocity;
+		ball_p = ball.transform.position;
+		light.transform.position = new Vector3(ball_p.x, 2.5f, ball_p.z);
+		if (ball_p.z >= -10 && ball_p.z <= 13) {
+			maincamera.transform.position = new Vector3 (ball_p.x, 2.5f, ball_p.z - 3f);	
+		}
+	}
+
+	void OnCollisionEnter(Collision collider){
+		if (collider.gameObject.transform.tag == "Garter" || collider.gameObject.transform.tag == "Stage") {
+			Vector3 force = new Vector3 (ball_v.x, ball_v.y, 10f);
+			ball.GetComponent<Rigidbody> ().velocity = force;
+	
+		}
+	}
+}
---
+++ VB2017/Assets/Scripts/Pin_sound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin_sound : MonoBehaviour {
+	private AudioSource audioSource;
+	public AudioClip sound;
+
+	void Start() {
+		audioSource        = gameObject.GetComponent<AudioSource>();
+		audioSource.loop    = false;    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCollisionEnter(Collision collider){
+		if (collider.gameObject.tag == "Ball") {
+			audioSource.PlayOneShot (sound);
+		}
+	}
+}
